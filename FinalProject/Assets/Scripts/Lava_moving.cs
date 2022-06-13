0fe315387ae1697fa826d468d46c5abc78d6853d
hugo.deplagne <hugo.deplagne@epita.fr>
2022-06-13 10:58:25 +0200

    All environment, lights do not work anymore...

--- FinalProject/Assets/Scripts/Lava_moving.cs
+++ FinalProject/Assets/Scripts/Lava_moving.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class Lava_moving : MonoBehaviour
 {
-    // Scroll main texture based on time
+    // Start is called before the first frame update
+    public float delayBetweenFrames;
 
-    float scrollSpeed = 0.1f;
-    float offset = 0f;
-    Renderer rend;
+    private Renderer rend;
+    private Material[] mat;  //Just in case you have more than one materials on a Renderer.
+    private float timeOfLastTextureShift;
 
     void Start()
     {
         rend = GetComponent<Renderer>();
+        mat = rend.materials;
     }
 
     void Update()
     {
-        offset += Time.deltaTime * scrollSpeed;
-        offset = offset % 1;
-        rend.material.SetTextureOffset("_MainTex", new Vector2(-offset, 0));
+        float timePassed = Time.timeSinceLevelLoad - timeOfLastTextureShift;
+        if (timePassed < delayBetweenFrames) return;
+
+
+        Vector2 newOffset = new Vector2(mat[0].GetTextureOffset("_BaseMap").x + 0.1f, 0);
+        rend.materials[0].SetTextureOffset("_BaseMap", newOffset);
+        timeOfLastTextureShift = Time.timeSinceLevelLoad;
     }
 }
