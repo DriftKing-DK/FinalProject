1e3678b788092cc0a75460445b7869c53d734855
hugo.deplagne <hugo.deplagne@epita.fr>
2022-06-14 20:56:59 +0200

    Added player to environment

---
+++ FinalProject/Assets/Scripts/Gun.cs
@@ -0,0 +1,165 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public int damage, magazineSize, bulletsPerTap;
+    public float timeBetweenShooting, spread, range, reloadTime, timeBetweenShots;
+    public bool allowButtonHold;
+
+    public float duration, magnitude, recoil;
+    
+    private float elapsed;
+    private int bulletsLeft, bulletsShot;
+    private bool shooting, readyToShoot, reloading, shaking;
+
+    //private List<Vector3> debugDirection;
+
+    private Camera fpsCam;
+
+
+    public void Setup(Camera c)
+    {
+        fpsCam = c;
+        elapsed = 0f;
+        bulletsLeft = magazineSize;
+        shooting = false;
+        readyToShoot = true;
+        reloading = false;
+        shaking = false;
+        //debugDirection = new List<Vector3>();
+    }
+
+    public int GetBulletLeft()
+    {
+        return bulletsLeft;
+    }
+
+    public bool CanChangeGun()
+    {
+        return !reloading && bulletsLeft > 0 && readyToShoot;
+    }
+
+    public int ManageInput()
+    {
+        int res = 0;
+        if (allowButtonHold)
+            shooting = Input.GetMouseButton(1);
+        else
+            shooting = Input.GetMouseButtonDown(1);
+        
+        if (Input.GetKeyDown(KeyCode.R) && !reloading && bulletsLeft < magazineSize && readyToShoot)
+        {
+            Reload();
+            res = 1;
+        }
+
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            bulletsShot = bulletsPerTap;
+            Shoot();
+            res = 2;
+        }
+        Shake();
+
+        //debug
+        /*foreach (Vector3 v in debugDirection)
+            Debug.DrawRay(fpsCam.transform.position, v * range);*/
+        
+        return res;
+    }
+
+    private void ResetReload()
+    {
+        reloading = false;
+        bulletsLeft = magazineSize;
+    }
+
+    private void Reload()
+    {
+        reloading = true; // forbid the player to reload again
+        Invoke("ResetReload", reloadTime);
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false; // forbid the player to continu fireing
+        bulletsLeft -= 1;
+        ShootBis(); // first shot
+        while (bulletsShot > 1) // shot all bullets per tap
+        {
+            Invoke("ShootBis", timeBetweenShots);
+            bulletsShot -= 1;
+        }
+        Invoke("ResetShot", timeBetweenShooting); // allow the player to shoot again
+    }
+
+    private void ShootBis()
+    {
+        LauchFlash();
+        ResetShake();
+        //spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        //direction with spread
+        Quaternion spreadAngle_x = Quaternion.AngleAxis(x, new Vector3(0, 1, 0));
+        Quaternion spreadAngle_y = Quaternion.AngleAxis(y, new Vector3(1, 0, 0));
+
+        Vector3 direction = spreadAngle_y * spreadAngle_x * fpsCam.transform.forward;
+
+        //raycast
+        RaycastHit rayHit;
+        if (Physics.Raycast(fpsCam.transform.position, direction, out rayHit, range))
+        {
+            Target target = rayHit.transform.GetComponent<Target>();
+            if (target != null)
+                target.TakeDamage(damage);
+        }
+
+        //debugDirection.Add(direction);
+    }
+
+    private void LauchFlash()
+    {
+        ParticleSystem[] muzzleFlashs = GetComponentsInChildren<ParticleSystem>();
+        foreach (ParticleSystem muzzle in muzzleFlashs)
+        {
+            Light l = muzzle.GetComponentInChildren<Light>();
+            l.enabled = true;
+            muzzle.Play();
+            l.enabled = false;
+        }
+    }
+
+    private void ResetShake()
+    {
+        elapsed = 0f;
+        shaking = true;
+    }
+
+    private void Shake()
+    {
+        if (elapsed >= duration)
+        {
+            shaking = false;
+            elapsed = 0f;
+        }
+        
+        if (shaking)
+        {
+            float x = Random.Range(-0.1f, 0.1f) * magnitude;
+            float y = Random.Range(-0.1f, 0.1f) * magnitude;
+            transform.localPosition = new Vector3(x, y, transform.localPosition.z - recoil * Time.deltaTime);
+            elapsed += Time.deltaTime;
+        }
+        else
+            transform.localPosition = Vector3.zero;
+    }
+}
---
+++ FinalProject/Assets/Scripts/GunShooter.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GunShooter : MonoBehaviour
+{
+    public GameObject[] gunList;
+    public Camera cam;
+    public Slider AmmoLeft;
+    public Slider TimeSlider;
+    
+    //private GameObject myGun;
+    private Gun myGun;
+
+    private bool timerNeedUpdate;
+    private int gunAction = 0;
+
+    private int currentGun = 0;
+    private int nbGuns;
+
+    void Start()
+    {
+        nbGuns = gunList.Length;
+        for (int i = 0; i < nbGuns; i++)
+        {
+            transform.GetChild(i).gameObject.GetComponent<Gun>().Setup(cam);
+            gunList[i].SetActive(false);
+        }
+
+        UpdateGunProperties();
+        UISetup();
+    }
+
+    private void Update()
+    {
+        ChangeGun(Input.GetKeyDown(KeyCode.N), Input.GetKeyDown(KeyCode.P));
+
+        gunAction = myGun.ManageInput();
+        UIManagement();
+    }
+
+    private void UISetup(bool reload = false, bool load = false)
+    {
+        if (reload) // reloading setup
+        {
+            //set red color
+            Color color = new Color(233f/255f, 79f/255f, 55f/255f);
+            TimeSlider.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = color;
+
+            timerNeedUpdate = true;
+            TimeSlider.maxValue = myGun.reloadTime;
+            TimeSlider.value = myGun.reloadTime;
+        }
+        else if (load) //loading setup
+        {
+            timerNeedUpdate = true;
+            TimeSlider.value = myGun.timeBetweenShooting;
+            AmmoLeft.value = myGun.GetBulletLeft();
+        }
+        else // init setup
+        {
+            //AmmoLeft slider
+            AmmoLeft.maxValue = myGun.magazineSize;
+            AmmoLeft.minValue = 0;
+            AmmoLeft.value = myGun.GetBulletLeft();
+
+            //TimeSlider
+
+            // set white color
+            Color color = new Color(255f/255f, 255f/255f, 255f/255f);
+            TimeSlider.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = color;
+
+            timerNeedUpdate = false;
+            TimeSlider.maxValue = myGun.timeBetweenShooting;
+            TimeSlider.minValue = 0;
+            TimeSlider.value = 0;
+        }
+    }
+
+    private void UIManagement()
+    {
+        if (gunAction == 1) // reload
+        {
+            UISetup(reload : true);
+        }
+        else if (gunAction == 2)
+        {
+            UISetup(load : true);
+        }
+
+        if (timerNeedUpdate)
+        {
+            if (TimeSlider.value == 0)
+            {
+                timerNeedUpdate = false;
+                UISetup();
+            }
+            else
+            {
+                TimeSlider.value -= Time.deltaTime;
+            }
+        }
+    }
+
+    private void UpdateGunProperties()
+    {
+        gunList[currentGun].SetActive(true);
+        myGun = transform.GetChild(currentGun).gameObject.GetComponent<Gun>();
+        AmmoLeft.maxValue = myGun.magazineSize;
+        AmmoLeft.value = myGun.GetBulletLeft();
+        UISetup();
+    }
+
+    private void ChangeGun(bool n, bool p)
+    {
+        if (n != p && myGun.CanChangeGun())
+        {
+            gunList[currentGun].SetActive(false);
+            if (n == true) // next gun
+            {
+                currentGun += 1;
+                if (currentGun >= nbGuns)
+                    currentGun = 0;
+            }
+            else // previous gun
+            {
+                currentGun -= 1;
+                if (currentGun < 0)
+                    currentGun = nbGuns - 1;
+            }
+            UpdateGunProperties();
+        }
+    }
+}
